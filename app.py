e50d1289892f857a3036e5f459d6601135c3aa96
Galina798 <galina7778@bk.ru>
2025-08-15 18:18:36 +0700

    radiobutton2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from my_module import study_entry
+from study_radiobutton import study_radio
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -7,5 +8,7 @@ if __name__ == "__main__":
     root.geometry("400x300")
 
     tk.Button(root, text="Изучение полей ввода", command=study_entry).pack(pady=20)
+    
+    tk.Button(root, text="Изучение радиокнопок", command=study_radio).pack(pady=20)
 
     root.mainloop()
